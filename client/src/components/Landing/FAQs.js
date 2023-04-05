6641b1e410dc26b2dc7173a9c624dacdec0e82a9
Tanish Goyal <92181886+Tancoder20@users.noreply.github.com>
2023-04-05 11:57:14 +0530

    Updated FAQ.js

--- client/src/components/Landing/FAQs.js
+++ client/src/components/Landing/FAQs.js
@@ -5,31 +5,24 @@ function FAQs() {
     <>
       <div className="min-h-screen">
         <div className="bg-white mx-12 p-6">
+          
           <div
             tabIndex={0}
-            className="mb-4 collapse border border-base-300 bg-base-100 hover:bg-gray-100 rounded-lg collapse-plus"
+            className="mb-4 collapse border border-base-300 bg-base-100 rounded-lg hover:bg-gray-100 collapse-plus"
           >
             <div className="collapse-title text-lg font-medium">
-              How MS(R) program is different than an MTech program?
+              What is the eligibility criteria for applying for PhD programme?
             </div>
             <div className="collapse-content">
-              <p>
-                The basic idea of two programs are different. MTech program is
-                oriented towards masters in engineering while MS(R) program is
-                oriented towards research. Due to the basic approach of two
-                programs, we see a couple of differences: <br />
-                <br />
-                (a) MTech program has more course work then MS(R). A typical
-                course work of MTech will spread over minimum two semesters.
-                <br />
-                (b) In MS(R) student spend most of the time in doing research.
-                Due to nature of research, a student may take more than two
-                years to graduate from MS(R) program.
-                <br />
-                (c) MS(R) is evaluated based on his/her research output
-                (typically in form of publications). While for MTech, final
-                project could be implementation or research, therefore,
-                publications is not mandatory.
+              <p>They are:<br/>
+                1. A candidate who has qualified for the award of Master's degree of this Institute or a recognised
+                Institute or University in the discipline as prescribed in the regulations of the Senate is eligible
+                to apply for the PhD programme of this Institute.
+                <br/>
+                2. A candidate who has qualified for the award of Bachelor's Degree in Engineering / Technology
+                with exceptionally good academic background in the discipline as prescribed in the regulations
+                of the Senate is also eligible to apply for PhD programme in Engineering/Technology of this
+                Institute. 
               </p>
             </div>
           </div>
@@ -39,28 +32,11 @@ function FAQs() {
             className="mb-4 collapse border border-base-300 bg-base-100 hover:bg-gray-100 rounded-lg collapse-plus"
           >
             <div className="collapse-title text-lg font-medium">
-              What are the career options after MS(R) or what are the job
-              opportunities after MS(R)
+              What are the requirements during the course of PhD programme?
             </div>
             <div className="collapse-content">
               <p>
-                Since MS(R) is research oriented, there are following
-                possibilities:
-                <br />
-                (a) Further studies, PhD: By the end of MS(R) you would already
-                have some publications. So it is easier to get admission in PhD.
-                <br />
-                (b) Industry <br />
-                (i) Research oriented profile: Based on type of research that
-                candidate have done during MS(R), s/he can apply to various
-                companies that have research labs in India, such as Microsoft
-                research, IBM research, HP research, Xerox research, Intel labs,
-                Google labs, etc. <br />
-                (ii) Research and Development profile: In computer science many
-                companies prefer a research oriented profile for research and
-                development jobs. As it the candidate bring better understanding
-                of the concern subject matter. This kind of profile generally
-                require some development experience during IIT Ropar. <br />
+                The academic programme leading to PhD degree is broad based and involves a minimum course credit requirement, comprehensive examination, synopsis seminar and thesis submission. The institute also encourages research in interdisciplinary areas through a system of joint supervision and interdepartmental  group activities.<br />
               </p>
             </div>
           </div>
@@ -70,16 +46,59 @@ function FAQs() {
             className="mb-4 collapse border border-base-300 bg-base-100 hover:bg-gray-100 rounded-lg collapse-plus"
           >
             <div className="collapse-title text-lg font-medium">
-              As MS(R) is new in IIT Ropar, will it have good placements?
+              How will students be evaluated during the course of PhD programme?
             </div>
             <div className="collapse-content">
               <p>
-                MS(R) students are eligible to sit in companies that visit for
-                campus placements. In computer science, in general good number
-                of companies visit campus. In addition to that, student may
-                approach companies that work in his area of research. Based on
-                student's expertise, companies may conduct off-campus interview
-                for them.
+                Student is supposed to do a literature survey in a particular area of interest (may or may not be tightly coupled to research proposal), and find out at least 5 papers. The selected papers should be regular papers from A*/A/B ranked conferences/journals published in last 10 years. There will be two kinds of evaluation: <br />
+                <br />
+                (a) WRITTEN COMPREHENSIVE: DC of the student will conduct a written exam based on the papers identified by student/DC. Scope of the written examination will be the content and underneath basics of the papers. The evaluation of the written exam would be done by DC members.
+                <br /><br/>
+                (b) ORAL COMPREHENSIVE: Student will present these papers in front of department faculty members and doctoral committee members.
+<br/>
+Doctoral committee will ask questions on basics as well as detail of the research problems
+being addressed in the given papers.
+<br/>
+Based on presentation and viva-voce, doctoral committee would decide marks in the oral
+comprehensive examination.
+                
+              </p>
+            </div>
+          </div>
+
+          <div
+            tabIndex={0}
+            className="mb-4 collapse border border-base-300 bg-base-100 hover:bg-gray-100 rounded-lg collapse-plus"
+          >
+            <div className="collapse-title text-lg font-medium">
+              In what fields, can PhD can be pursued?
+            </div>
+            <div className="collapse-content">
+              <p>
+                We, at IIT Ropar, provide the facility for research work leading to PhD degree in all the following departments:
+                <br/>
+                1. Department of Biomedical Engineering
+                <br/>
+                2. Department of Metallurgical and Materials Engineering
+                <br/>
+                3. Department of Chemical Engineering
+                <br/>
+                4. Department of Chemistry
+                <br/>
+                5. Department of Civil Engineering
+                <br/>
+                6. Department of Computer Science & Engineering
+                <br/>
+                7. Department of Electrical Engineering
+                <br/>
+                8. Department of Humanities & Social Sciences
+                <br/>
+                9. Department of Mathematics
+                <br/>
+                10. Department of Mechanical Engineering
+                <br/>
+                11. Department of Physics  
+                <br/>
               </p>
             </div>
           </div>
@@ -89,33 +108,18 @@ function FAQs() {
             className="mb-4 collapse border border-base-300 bg-base-100 rounded-lg hover:bg-gray-100 collapse-plus"
           >
             <div className="collapse-title text-lg font-medium">
-              Is it actually better to do MS(R) or MTech
+              From where can I access the courses in PhD provided by respective departments?
             </div>
             <div className="collapse-content">
               <p>
-                As a rule of thumb, to build research profile, one should do
-                MS(R) and to increase the industry job prospects one should do
-                MTech. However, this rule of thumb is not very hard rule.
-                Students after finishing MS(R) may go for industry jobs, and PhD
-                can be done after finishing MTech.
+                Here's this direct link to the required page: 
+                <a href="https://www.iitrpr.ac.in/phd-courses" target="_blank"
+                rel="noopener noreferrer"
+                style={{ color: "rgb(88 80 236)" }}> Link</a>
               </p>
             </div>
           </div>
 
-          <div
-            tabIndex={0}
-            className="mb-4 collapse border border-base-300 bg-base-100 rounded-lg hover:bg-gray-100 collapse-plus"
-          >
-            <div className="collapse-title text-lg font-medium">
-              What are the research areas in the field of MS?
-            </div>
-            <div className="collapse-content">
-              <p>
-                For research areas, one should look at the individual faculty
-                profile of computer science department.
-              </p>
-            </div>
-          </div>
         </div>
       </div>
     </>
